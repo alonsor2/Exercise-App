4208b30f90b53f5269a836c05f606f5166c17027
Robert Alonso <alonsor2@hawkmail.newpaltz.edu>
2018-11-29 21:03:14 -0500

    Adding needed dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="container">
+    <div class = "display-3">
+      <img alt="Vue logo" src="./assets/logo.png" id="logo">
+        StayFit
     </div>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+#logo { height: 50px}
 </style>
+
+<script>
+// @ is an alias to /src
+import Nav from '@/components/Nav.vue'
+export default {
+  name: 'home',
+  components: {
+    Nav
+  }
+}
+</script>
