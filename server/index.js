336b8c8fc1cdaf29d79e20e4c9ca6f561d9f43b5
Robert Alonso <alonsor2@hawkmail.newpaltz.edu>
2018-12-01 11:02:33 -0500

    Client/Server Communication(Api Access file)

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const path = require('path');
 const application = require('./app/controller');
-const port = 3000;
+const port = 80;
+const server = "localhost";
 const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+  });
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use("/", express.static(__dirname + "/../dist/"));
 app.use('/application', application);
+app.use("/", (req, res) => res.sendFile(path.join(__dirname + "/../dist/index.html")));
 
 app.listen(port);
+
+// eslint-disable-next-line no-console
+console.log(`listening on: http://${server}:${port}`);
---
+++ src/services/api_access.js
@@ -0,0 +1,34 @@
+const api_root = "http://localhost:80/";
+export let userId = null;
+
+export function GetState(){
+    return myFetch(api_root + "/");
+}
+
+
+
+
+function myFetch(url = ``, data = null) {
+    let options = {
+          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: "same-origin", // include, same-origin, *omit
+          headers: {
+              playerId: playerId
+          }
+    };
+    if(data){
+        options = { 
+          ...options,
+          method:  "POST", // *GET, POST, PUT, DELETE, etc.
+          headers: {
+              ...options.headers,
+              "Content-Type": "application/json; charset=utf-8",
+              // "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: JSON.stringify(data), // body data type must match "Content-Type" header
+        };
+    }
+    return fetch(url, options)
+    .then(response =>{
+      return response.json()
+    }); // parses response to JSON
